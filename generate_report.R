457c8ed03e5d286757797ef0673ab5a39580bebb
bnjmn <benja_e_e@hotmail.com>
2024-06-19 00:15:40 -0400

    up to report

--- generate_report.R
+++ generate_report.R
@@ -7,7 +7,7 @@ input_dir <- './report'
 
 # Verificar que la carpeta existe y listar los archivos
 if (dir.exists(input_dir)) {
-  
+
   # Listar todos los archivos .txt en el directorio
   files <- list.files(input_dir, pattern = '\\.txt$', full.names = TRUE)
   
@@ -18,15 +18,26 @@ if (dir.exists(input_dir)) {
     output_rmd <- 'reporte_proyecto.Rmd'
     
     # Crear el archivo RMarkdown
-    cat('# Reporte de Archivos de Texto\n', file = output_rmd)
+    cat('---\n', file = output_rmd)
+    cat('title: "Reporte de Archivos de Texto"\n', file = output_rmd, append = TRUE)
+    cat('author: "Tu Nombre"\n', file = output_rmd, append = TRUE)
+    cat('date: "`r Sys.Date()`"\n', file = output_rmd, append = TRUE)
+    cat('output: pdf_document\n', file = output_rmd, append = TRUE)
+    cat('---\n\n', file = output_rmd, append = TRUE)
+    
+    cat('# Reporte de Archivos de Texto\n', file = output_rmd, append = TRUE)
     
     # Leer cada archivo y agregar su contenido al archivo RMarkdown
     for (file in files) {
       cat('\n\n', file = output_rmd, append = TRUE)
       cat(paste0('## ', basename(file), '\n'), file = output_rmd, append = TRUE)
-      cat('```{r, results="asis"}\n', file = output_rmd, append = TRUE)
-      cat(paste0('cat(paste(readLines(', shQuote(file), '), collapse = "\\n"))\n'), file = output_rmd, append = TRUE)
-      cat('```\n', file = output_rmd, append = TRUE)
+      cat('```{verbatim}\n', file = output_rmd, append = TRUE)
+      
+      # Leer y escribir el contenido del archivo preservando los saltos de línea
+      txt_content <- readLines(file)
+      cat(paste(txt_content, collapse = "\n"), file = output_rmd, append = TRUE)
+      
+      cat('\n```\n', file = output_rmd, append = TRUE)
     }
     
     # Compilar el archivo RMarkdown a PDF
